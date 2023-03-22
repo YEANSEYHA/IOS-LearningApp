9e7290d716bc647bf357f379074df27e11b9d9cf
Yean SEYHA <yeanseyha7@gmail.com>
2023-03-22 11:34:46 +0700

    HomeviewRow

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,11 +12,47 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
+        NavigationView {
+            VStack(alignment: .leading){
+                Text("What do you want to do today ?")
+                    .padding(.leading,20)
+                
+                ScrollView{
+                    LazyVStack{
+                        ForEach(model.modules) {
+                            module in
+                            // Learning Card
+                            VStack(spacing:20){
+                                HomeViewRow(
+                                    image: module.content.image,
+                                    title: "learn\(module.category)",
+                                    description: module.content.description,
+                                    count: "\(module.content.lessons.count) Lessons",
+                                    time: module.content.time
+                                    
+                                )
+                                // Test Card
+                                HomeViewRow(
+                                    image: module.test.image,
+                                    title: "\(module.category) Test",
+                                    description: module.test.description,
+                                    count: "\(module.test.questions.count) Lessons",
+                                    time: module.test.time
+                                    
+                                )
+
+                            }
+                                                        
+                        }
+                    }.padding()
+                }
+                }
+            .navigationTitle("Get Started")
+            }
+        }
         
-        Text("Hello, world! change")
-            .padding()
+        
     }
-}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
